e3634899b1f175243ae18335efa41f3fbad1fef1
Markus Schuh <markus.schuh@capgemini.com>
2018-11-28 02:44:17 +0100

    New versions for JSR105 and Santuario API - aligned as far as possible

---
+++ compare/CreateSignatureJSR105.java
@@ -0,0 +1,186 @@
+/**
+ * This is a simple example of generating an Enveloped XML
+ * Signature using the JSR 105 API. The resulting signature will look
+ * like (key and signature values will be different):
+ *
+ * <pre><code>
+ *<Envelope xmlns="urn:envelope">
+ * <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
+ *   <SignedInfo>
+ *     <CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
+ *     <SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"/>
+ *     <Reference URI="">
+ *       <Transforms>
+ *         <Transform Algorithm="http://www.w3.org/2000/09/xmldsig#enveloped-signature"/>
+ *          <Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
+ *       </Transforms>
+ *       <DigestMethod Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
+ *       <DigestValue>K8M/lPbKnuMDsO0Uzuj75lQtzQI=<DigestValue>
+ *     </Reference>
+ *   </SignedInfo>
+ *   <SignatureValue>
+ *     DpEylhQoiUKBoKWmYfajXO7LZxiDYgVtUtCNyTgwZgoChzorA2nhkQ==
+ *   </SignatureValue>
+ *   <KeyInfo>
+ *     <KeyValue>
+ *       <RSAKeyValue>
+ *        <Modulus>ygPSo3j0GR6U4obxWT82fBsIgZevUDlsS37oDvaxRY3dn5lqvgCQw8IUP2BcUdV9j6bglymBfkR9
+72FjgMHdi+mNxQaP2emOxNRb+HiiToLCPbjZWiRrgVOZedlDrAXIJeBFaPRA0ZaeReNJk3h2qDEZ
+3JBcNa2hDVr6i3VJc8NPVCM9yn6tVPxRISSZRYORIdul7uJB20aAeQG8pWwFtpTv2dxjxgKc7XpY
+QCh+fb/or8ovMCOU6QHJ0WeeYNNvN7oPOpzr5N7aEK6/oiKd1cqTs4onArd0eEvG+Z0LfeUhE+BO
+N+lhE7x6wrvM49/lcMWoopMJcqq07MyGYNbC6Q==</Modulus>
+ *         <Exponent>AQAB</Exponent>
+ *       </RSAKeyValue>
+ *     </KeyValue>
+ *   </KeyInfo>
+ * </Signature>
+ *</Envelope>
+ * </code></pre>
+ */
+
+import javax.xml.crypto.dsig.CanonicalizationMethod;
+import javax.xml.crypto.dsig.DigestMethod;
+import javax.xml.crypto.dsig.Reference;
+import javax.xml.crypto.dsig.SignedInfo;
+import javax.xml.crypto.dsig.Transform;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMSignContext;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+import javax.xml.crypto.dsig.keyinfo.KeyValue;
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+public class CreateSignatureJSR105 {
+
+    //
+    // Synopsis: java CreateSignatureJSR105  [document] [output]
+    //
+    //    where "document" is the name of a file containing the XML document
+    //    to be signed, and "output" is the name of the file to store the
+    //    signed document. The 2nd argument is optional - if not specified,
+    //    standard output will be used.
+    //
+    public static void main(String[] args) throws Exception {
+
+        //All the parameters for the keystore
+        String keystoreType     = "JKS";
+        String keystoreFile     = "build/envelope.keystore";
+        String keystorePass     = "my-password";
+        String privateKeyAlias  = "envelope";
+        String privateKeyPass   = "my-password";
+        String certificateAlias = "envelope";
+
+        KeyStore ks = KeyStore.getInstance(keystoreType);
+        FileInputStream fis = new FileInputStream(keystoreFile);
+        
+        //load the keystore
+        ks.load(fis, keystorePass.toCharArray());
+
+        //get the private key for signing
+        PrivateKey privateKey = (PrivateKey) ks.getKey(privateKeyAlias, privateKeyPass.toCharArray());
+
+        PublicKey publicKey = ks.getCertificate(certificateAlias).getPublicKey();
+        KeyPair kp = new KeyPair(publicKey, privateKey);
+
+        // Instantiate the document to be signed
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new FileInputStream(args[0]));
+
+        Element root = doc.getDocumentElement();
+
+        // Create a DOM XMLSignatureFactory that will be used to generate the enveloped signature
+        XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+
+        // Use RSA-256 as algorithm for digital signature
+        SignatureMethod signatureMethod = fac.newSignatureMethod("http://www.w3.org/2001/04/xmldsig-more#rsa-sha256", null);
+
+        // Create the list of transformations for the Document/Reference
+        final List<Transform> transforms = new ArrayList<Transform>(2);
+        transforms.add(fac.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null));
+        transforms.add(fac.newTransform(CanonicalizationMethod.EXCLUSIVE, (TransformParameterSpec) null));
+
+        Reference ref = fac.newReference
+            ("", fac.newDigestMethod(DigestMethod.SHA256, null),
+             transforms,
+             null, null);
+
+        // Create the SignedInfo
+        SignedInfo si = fac.newSignedInfo
+            (fac.newCanonicalizationMethod
+             (CanonicalizationMethod.EXCLUSIVE,
+              (C14NMethodParameterSpec) null),
+             signatureMethod,
+             Collections.singletonList(ref));
+
+        // Remove any old Signature node
+        NodeList nodes = doc.getElementsByTagName("Signature");
+        for (int i = 0; i < nodes.getLength(); i++) {
+          Node signaturinfonode = nodes.item(i);
+          signaturinfonode.getParentNode().removeChild(signaturinfonode);
+        }
+
+        // Create a KeyValue containing the RSA PublicKey
+        KeyInfoFactory kif = fac.getKeyInfoFactory();
+        KeyValue kv = kif.newKeyValue(kp.getPublic());
+        // Create a KeyInfo and add the KeyValue to it
+        KeyInfo ki = kif.newKeyInfo(Collections.singletonList(kv));
+        // Create the XMLSignature (but don't sign it yet)
+        XMLSignature signature = fac.newXMLSignature(si, ki);
+
+        // Create a DOMSignContext and specify the RSA PrivateKey and
+        // location of the resulting XMLSignature's parent element
+        DOMSignContext dsc = new DOMSignContext(kp.getPrivate(), root);
+        // sign the enveloped signature
+        signature.sign(dsc);
+
+        // output the resulting document
+        OutputStream os;
+        if (args.length > 1) {
+           os = new FileOutputStream(args[1]);
+        } else {
+           os = System.out;
+        }
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer trans = tf.newTransformer();
+        trans.transform(new DOMSource(doc), new StreamResult(os));
+
+        os.close();
+
+    }
+}
---
+++ compare/CreateSignatureSantuario.java
@@ -0,0 +1,151 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.xml.security.algorithms.MessageDigestAlgorithm;
+import org.apache.xml.security.algorithms.SignatureAlgorithm;
+import org.apache.xml.security.c14n.Canonicalizer;
+import org.apache.xml.security.keys.KeyInfo;
+import org.apache.xml.security.signature.ObjectContainer;
+import org.apache.xml.security.signature.SignedInfo;
+import org.apache.xml.security.signature.XMLSignature;
+import org.apache.xml.security.test.dom.DSNamespaceContext;
+import org.apache.xml.security.transforms.Transforms;
+import org.apache.xml.security.transforms.params.XPath2FilterContainer;
+import org.apache.xml.security.transforms.params.XPathContainer;
+import org.apache.xml.security.utils.Constants;
+import org.apache.xml.security.utils.ElementProxy;
+import org.apache.xml.security.utils.XMLUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class CreateSignatureSantuario {
+
+    static org.slf4j.Logger LOG =
+        org.slf4j.LoggerFactory.getLogger(CreateSignatureSantuario.class);
+
+    //
+    // Synopsis: java CreateSignatureSantuario [document] [output]
+    //
+    //    where "document" is the name of a file containing the XML document
+    //    to be signed, and "output" is the name of the file to store the
+    //    signed document. The 2nd argument is optional - if not specified,
+    //    standard output will be used.
+    //
+    public static void main(String[] args) throws Exception {
+
+        org.apache.xml.security.Init.init();
+        ElementProxy.setDefaultPrefix(Constants.SignatureSpecNS, "");
+
+        //All the parameters for the keystore
+        String keystoreType     = "JKS";
+        String keystoreFile     = "build/envelope.keystore";
+        String keystorePass     = "my-password";
+        String privateKeyAlias  = "envelope";
+        String privateKeyPass   = "my-password";
+        String certificateAlias = "envelope";
+
+        KeyStore ks = KeyStore.getInstance(keystoreType);
+        FileInputStream fis = new FileInputStream(keystoreFile);
+
+        //load the keystore
+        ks.load(fis, keystorePass.toCharArray());
+
+        //get the private key for signing
+        PrivateKey privateKey = (PrivateKey) ks.getKey(privateKeyAlias, privateKeyPass.toCharArray());
+
+        PublicKey publicKey = ks.getCertificate(certificateAlias).getPublicKey();
+        KeyPair kp = new KeyPair(publicKey, privateKey);
+
+        // Instantiate the document to be signed
+        DocumentBuilder db = XMLUtils.createDocumentBuilder(false);
+        Document doc = db.parse(new FileInputStream(args[0]));
+
+        Element root = doc.getDocumentElement();
+
+        // Use RSA-256 as algorithm for digital signature
+        SignatureAlgorithm signatureAlgorithm =
+          new SignatureAlgorithm(doc, XMLSignature.ALGO_ID_SIGNATURE_RSA_SHA256);
+
+        // Create the list of transformations for the Document/Reference
+        Transforms transforms = new Transforms(doc);
+        transforms.addTransform(Transforms.TRANSFORM_ENVELOPED_SIGNATURE);
+        transforms.addTransform(Transforms.TRANSFORM_C14N_EXCL_OMIT_COMMENTS);
+
+        Element canonElem =
+            XMLUtils.createElementInSignatureSpace(doc, Constants._TAG_CANONICALIZATIONMETHOD);
+        canonElem.setAttributeNS(
+            null, Constants._ATT_ALGORITHM, Canonicalizer.ALGO_ID_C14N_EXCL_OMIT_COMMENTS
+        );
+
+        // Create a XMLSignature that will be used to generate the enveloped signature
+        XMLSignature signature =
+          new XMLSignature(doc, null, signatureAlgorithm.getElement(), canonElem);
+
+        //Add the above Document/Reference
+        signature.addDocument("", transforms, MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA256);
+
+        // Remove any old Signature node
+        NodeList nodes = doc.getElementsByTagName("Signature");
+        for (int i = 0; i < nodes.getLength(); i++) {
+          Element signaturinfonode = (Element)nodes.item(i);
+          signaturinfonode.getParentNode().removeChild(signaturinfonode);
+        }
+
+        signature.addKeyInfo(kp.getPublic());
+
+        root.appendChild(signature.getElement());
+        // sign the enveloped signature
+        signature.sign(kp.getPrivate());
+
+        // output the resulting document
+        OutputStream os;
+        if (args.length > 1) {
+           os = new FileOutputStream(args[1]);
+        } else {
+           os = System.out;
+        }
+
+        XMLUtils.outputDOM(doc, os);
+
+        os.close();
+
+    }
+}
